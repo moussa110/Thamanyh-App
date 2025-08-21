bfaf8d53a820bb966dd56c1c0fdc4e329ebdf2db
Mahmoud Moussa <mahmoud.mousa.dev@gmail.com>
2025-08-21 22:15:14 +0300

    - Setup Dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android)
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
@@ -49,6 +51,25 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation.compose)
+    ksp(libs.hilt.compiler)
+
+    // Retrofit
+    implementation(libs.bundles.retrofit)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Coil for images
+    implementation (libs.bundles.coil)
+
+    // For LazyHorizontalGrid
+    implementation (libs.androidx.foundation)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
